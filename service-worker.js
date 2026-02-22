cd4e3287f913cbabfe06514ed2b6b3e37b75473a
ghostmbgamer-commits <ghostmbgamer@gmail.com>
2026-02-22 17:37:43 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,21 @@
-self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('install', event => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
-    })
+    }).catch(() => caches.match('/index.html'))
   );
+});
+
+self.addEventListener('message', event => {
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+  }
 });
